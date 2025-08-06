ee08ee1be873815907d7ad2874bafe9d7917c574
Digital-Media-Group <119053220+Digital-Media-Group@users.noreply.github.com>
2025-08-06 18:39:45 +0200

    Create quotreminder.php

---
+++ modules/addons/quotreminder/quotreminder.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Quote Reminder para WHMCS
+ * Desarrollado por Digital Media Group - https://digitalmediagroup.es
+ * GitHub: https://github.com/tuusuario/quotreminder-whmcs-addon
+ * 
+ * Licencia: MIT
+ */
